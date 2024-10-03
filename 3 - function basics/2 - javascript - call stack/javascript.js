7f3b1fafda3dfbfc9e119c9a50fd83912fcaab6f
Alexander Mills <alexander.mills@gtri.gatech.edu>
2024-10-03 16:22:52 -0400

    added explanation and rearranged content

--- 3 - function basics/2 - javascript - call stack/javascript.js
+++ 3 - function basics/2 - javascript - call stack/javascript.js
@@ -1,3 +1,20 @@
+
+/* 
+    Notes:
+    I wasn't familiar with creating delays in JavaScript so these sources helped:
+        - setTimeout: https://developer.mozilla.org/en-US/docs/Web/API/setTimeout 
+        - async, await, and promises: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
+
+    At some point, the value of para ended up being Promise Object
+        1. Pointed me in right direction: https://stackoverflow.com/questions/46719974/promise-is-returning-object-promise
+      
+    At some point, I was getting NaN as para's value
+        Then I ended up asking ChatGPT, which suggested that I needed to wait for 
+        for calculations to resolve before returning them or using them for anything.
+            So I added an await to the add and assigned it to a constant BEFORE returning it
+                - This resolved the issue
+        */
+
 const aInput = document.querySelector(".numberInputA");
 const bInput = document.querySelector(".numberInputB");
 const button = document.querySelector("button");
@@ -5,7 +22,7 @@ const para = document.querySelector(".output");
 const list = document.querySelector("ul");
 
 
-let stack = [add.name, average.name]
+let stack = [add.name, average.name, "javascript.js"]
 
 async function add(a,b) {
     await push();
@@ -19,12 +36,13 @@ async function average(a,b) {
 }
 
 button.addEventListener('click', async () => {
+    await push();
     const numA = parseFloat(aInput.value);
     const numB = parseFloat(bInput.value);
     const result = await average(numA, numB);
-    para.textContent = result;
+    para.textContent = "Result: " + result;
     poper();
-    stack = [add.name, average.name]
+    stack = [add.name, average.name, "javascript.js"]
 });
 
 function updateStack() {
